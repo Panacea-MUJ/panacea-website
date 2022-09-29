e1886a84e9e1690fa053bd3cb0e75f03584534c0
RavishGupta <ravishgupta2004.coder@gmail.com>
2022-09-29 23:17:57 +0530

    added cards

--- panacea/src/App.js
+++ panacea/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import Events from "./components/Events";
 
 function App() {
   return (
     <div>
       <Navbar />
-      
+      <Events />
     </div>
   );
 }
